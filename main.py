3eaf01d5a3b86f47c5363df85b236bb1085ecc40
Facundo Juarez <facundojuarezf@outlook.com>
2023-03-09 21:55:40 -0300

    Agrega soporte para rgb

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
+import sys
+from skimage.exposure import rescale_intensity
 import numpy as np
-from numpy import ndarray
 import cv2
-from cv2 import Mat
+import filters
 
 
 def main():
     # Cargar imagen original
     image = cv2.imread('./source.jpg')
-    image_gray_scale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    small_blur = np.ones(shape=(7, 7), dtype="float") * (1.0 / (7 * 7))
+    if image is None or image.size == 0:
+        sys.exit("Imagen no válida")
 
     # Aplicar el filtro
-    result_image = convolve(image, small_blur)
+    filtered_image = convolve(image, filters.identity)
 
-    # Mostrar imágenes
+    render_image(filtered_image)
+
+    sys.exit(0)
+
+
+def render_image(image):
     cv2.namedWindow("original_image", cv2.WINDOW_KEEPRATIO)
-    cv2.imshow("original_image", image_gray_scale)
-    cv2.imshow("original_image", result_image)
+    cv2.imshow("original_image", image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
-def convolve(image: Mat, kernel: ndarray) -> Mat:
+def convolve(image, kernel):
     # Obtener las dimensiones de la imagen y del kernel
     (image_height, image_width) = image.shape[:2]
     (kernel_height, kernel_width) = kernel.shape[:2]
+    channels = image.shape[-1] if image.ndim == 3 else 1
 
     # Padding para los bordes horizontales y verticales de la imagen
     h_pad = (kernel_width - 1) // 2
@@ -36,11 +42,11 @@ def convolve(image: Mat, kernel: ndarray) -> Mat:
                                left=h_pad, right=h_pad, borderType=cv2.BORDER_REPLICATE)
 
     # Crear matriz nula
-    output = np.zeros((image_height, image_width), dtype="float32")
+    output = np.zeros((image_height, image_width, channels))
 
-    # Recorrer la imagen de izquierda a derecha y desde arriba hacia abajo aplicando el kernel a cada pixel
+   # Recorrer la imagen de izquierda a derecha y desde arriba hacia abajo aplicando el kernel a cada pixel
     for y in np.arange(v_pad, image_height + v_pad):
-        for x in np.arange(v_pad, image_width + v_pad):
+        for x in np.arange(h_pad, image_width + h_pad):
             # Obtener la región de la imagen sobre la cual se aplicara la convolución
             y_start = (y - v_pad)
             y_end = (y + v_pad + 1)
@@ -49,17 +55,20 @@ def convolve(image: Mat, kernel: ndarray) -> Mat:
 
             roi = image[y_start:y_end, x_start:x_end]
 
-            # perform the actual convolution by taking the
-            # element-wise multiplicate between the ROI and
-            # the kernel, then summing the matrix
-            k = (roi * kernel).sum()
-            # store the convolved value in the output (x,y)-
-            # coordinate of the output image
-            output[y, x] = k
+            # Aplicar la convolución a cada canal
+            for channel_index in range(channels):
+                # Obtener el canal (r, g, b)
+                channel_values = roi[:, :, channel_index]
 
-    normalized_image = cv2.normalize(output,  output, 0, 255, cv2.NORM_MINMAX)
+                # Aplicar la convolución
+                k = (channel_values * kernel).sum()
 
-    return normalized_image
+                output[y-v_pad, x-h_pad][channel_index] = k
+
+    output = rescale_intensity(output, in_range=(0, 255))
+    output = (output * 255).astype("uint8")
+
+    return output
 
 
 if __name__ == '__main__':
